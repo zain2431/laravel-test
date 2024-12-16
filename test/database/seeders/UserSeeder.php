c163493c33d2f23509a487a18faf04e64459be52
baqir <baqir@aalasolutions.com>
2024-12-16 17:21:35 +0500

    Seeder created for user

---
+++ test/database/seeders/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create(['name'=>'Admin','email'=>'admin@admin.com','password'=>'password']);
+    }
+}
